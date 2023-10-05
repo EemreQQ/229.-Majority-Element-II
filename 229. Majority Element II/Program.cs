f48e70dd96444a87ec04468046cce9fe4750a5d5
Emre <emre_erol17@hotmail.com>
2023-10-05 10:55:11 +0300

    Proje dosyası ekle.

---
+++ 229. Majority Element II/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public IList<int> MajorityElement(int[] nums)
+    {
+        Dictionary<int, int> d = new Dictionary<int, int>();
+        List<int> list = new List<int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (d.Keys.Contains(nums[i])) 
+                d[nums[i]]++;
+            else 
+                d.Add(nums[i], 1);
+            if (d[nums[i]] > nums.Length / 3 && list.Contains(nums[i]) == false)               
+                list.Add(nums[i]);
+        }
+        return list;
+    }
+}
